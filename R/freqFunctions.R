72072abbfcabbe3218240766504140d8db533eec
McTwiszt <lahjouji@gmx.de>
2022-12-14 13:45:20 +0100

    Update getTokenFreqsRegex
    Updated getTokenFreqsRegex

--- R/freqFunctions.R
+++ R/freqFunctions.R
@@ -215,7 +215,7 @@ getTokenFreqsRegex <- function(type = "w", size = 2, token = "^\\bу\\b.*", toke
     freqlist_scaled <- as.data.frame(scale(freqlist[,2:ncol(freqlist)]))
     freqlist <- cbind(freqlist[,1], freqlist_scaled)
     colnames(freqlist)[1] <- "Speaker"
-    results <<- regexResult <- as.data.frame(freqlist[ , grepl( token , names( freqlist ), perl = perl ) ], styloFreqList_w_2$Speaker) %>% tibble::rownames_to_column(., "Speaker")
+    results <<- regexResult <- as.data.frame(freqlist[ , grepl( token , names( freqlist ), perl = perl ) ], freqlist$Speaker) %>% tibble::rownames_to_column(., "Speaker")
     if(size >1){
       regexResult$NewCol <- as.numeric(apply(regexResult[,1:ncol(regexResult)], 1, sum))
       subset <- cbind(freqlist[,1], regexResult)
@@ -257,7 +257,7 @@ getTokenFreqsRegex <- function(type = "w", size = 2, token = "^\\bу\\b.*", toke
   }
   
   else{
-    results <<- regexResult <- as.data.frame(freqlist1[ , grepl( token , names( freqlist1 ), perl = perl ) ] , styloFreqList_w_2$Speaker) %>% tibble::rownames_to_column(., "Speaker")
+    results <<- regexResult <- as.data.frame(freqlist1[ , grepl( token , names( freqlist1 ), perl = perl ) ] , freqlist$Speaker) %>% tibble::rownames_to_column(., "Speaker")
     if(size >1){
       regexResult$NewCol <- as.numeric(apply(regexResult[,1:ncol(regexResult)], 1, sum))
       subset <- cbind(freqlist1[,1], regexResult)
