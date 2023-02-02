969d3b65f11342508f3f1ac744ab5e3c377744f3
McTwiszt <lahjouji@gmx.de>
2023-02-02 14:59:52 +0100

    Update freqFunctions.R
    getAllStyloFreqs added
    
    increase of list cutoff möglich

--- R/freqFunctions.R
+++ R/freqFunctions.R
@@ -10,7 +10,7 @@
 #' @examples
 #' getStyloFreqList()
 #' 
-getStyloFreqList <- function(type = "w", size = 1, path, corpusfoldername = "corpus",  mfw.min = 100, mfw.max = 500, mfw.incr = 100, culling.min = 20, culling.max = 70, culling.incr = 20, distance.measure = "eder",  output = freq_list){
+getStyloFreqList <- function(type = "w", size = 1, path, corpusfoldername = "corpus",  mfw.min = 100, mfw.max = 500, mfw.incr = 100, culling.min = 20, culling.max = 70, culling.incr = 20, mfw.list.cutoff = 5000, distance.measure = "eder",  output = freq_list){
   # This function creates a stylo frequency list based on predefined settings (type = word or character, size = n-gram size, most frequent words, culling and distance measure.
   # The created frequencylist will automatically be 
   setwd(path)
@@ -29,7 +29,7 @@ getStyloFreqList <- function(type = "w", size = 1, path, corpusfoldername = "cor
         culling.min = {{culling.min}},
         culling.max = {{culling.max}},
         culling.incr = {{culling.incr}},
-        mfw.list.cutoff = 5000,
+        mfw.list.cutoff = {{mfw.list.cutoff}},
         delete.pronouns = FALSE,
         use.existing.freq.tables = FALSE,
         use.existing.wordlist = FALSE,
@@ -323,4 +323,160 @@ plotStyloFreqs <- function(df, x = var, y = value, fill = var, title = "plot", x
     suppressWarnings(print(melt_plot))
   }
 }
+
+
+#' @export
+getAllTokenFreqs <- function(type = "w", size = 1, token = "у", scale = F){
+  name <- gsub(" ","", paste("styloFreqList","_", type, "_", size))
+  freqlist1 <- eval(as.symbol(name))
+  if(scale == T){
+    subset0 <- as.data.frame(freqlist1)
+    subset0 <- data.frame("Speaker" = subset0$Speaker, Token = subset0[, token], check.names = F)
+    results <<- subset0 <- subset(subset0, Token >= 0)
+    freqlist_scaled <- as.data.frame(scale(subset0[,2:ncol(subset0)]))
+    dataframe <- cbind(subset0[,1], freqlist_scaled)
+    colnames(dataframe)[1] <- "Speaker"
+    colnames(dataframe)[2] <- "Token_Scaled"
+    dataframe2 <-dataframe[order(dataframe$Token_Scaled, decreasing = T),]
+    dataframe2$var <- dplyr::case_when(grepl("LEM", dataframe2$Speaker) ~ "Lemko", 
+                                       grepl("TRA", dataframe2$Speaker) ~ "Transcarpathian",
+                                       grepl("SLO", dataframe2$Speaker) ~ "Slovak")
+    
+    subset2 <- as.data.frame(freqlist1)
+    dataframe22 <- data.frame("Speaker" = subset2$Speaker, Token = subset2[, token], check.names = F)
+    dataframe22 <- subset(dataframe22, Token >=0)
+    dataframe222 <-dataframe22[order(dataframe22$Token, decreasing = T),] 
+    
+    dataframe3 <- cbind(dataframe2, dataframe222$Token)
+    names(dataframe3)[names(dataframe3) == "dataframe222$Token"] <- "Token"
+  }
+  else{
+    subset <- as.data.frame(freqlist1)
+    dataframe <- data.frame("Speaker" = subset$Speaker, Token = subset[, token], check.names = F)
+    results <<- dataframe <- subset(dataframe, Token >= 0)
+    dataframe2 <-dataframe[order(dataframe$Token, decreasing = T),] 
+    dataframe2$var <- dplyr::case_when(grepl("LEM", dataframe2$Speaker) ~ "Lemko", 
+                                       grepl("TRA", dataframe2$Speaker) ~ "Transcarpathian",
+                                       grepl("SLO", dataframe2$Speaker) ~ "Slovak")
+    dataframe3 <- dataframe2
+  }
+  names(dataframe3)[names(dataframe3) == "Token"] <- token
+  names(dataframe3)[names(dataframe3) == "Token_Scaled"] <- gsub(" ","", paste(token, "_scaled"))
+  return(dataframe3)
+}
+
+
+debug(getAllTokenFreqs)
+
+znam <- getAllTokenFreqs(type = "w", size = 1, token = "znam", scale = F)
+znam <- getTokenFreqs(type = "w", size = 1, token = "znam", scale = T)
+
+
+#' @export
+getAllTokenFreqsRegex <- function(type = "w", size = 2, token = "^\\bу\\b.*", tokenInWords = "", scale = F, perl = F){
+  name <- gsub(" ","", paste("styloFreqList","_", type, "_", size))
+  freqlist1 <- eval(as.symbol(name))
+  if(scale == T){
+    freqlist0 <- as.data.frame(freqlist1)
+    regexResult0 <- as.data.frame(freqlist0[ , grepl( token , names( freqlist0 ) ) ])
+    regexResult0 <- cbind(freqlist0[,1],regexResult0)
+    colnames(regexResult0)[1] <- "Speaker"
+    regexResult0$NewCol <- as.numeric(apply(regexResult0[,2:ncol(regexResult0)], 1, sum))
+    freqlist <- subset(regexResult0, NewCol >= 0)
+    freqlist$NewCol <- NULL
+    freqlist_scaled <- as.data.frame(scale(freqlist[,2:ncol(freqlist)]))
+    freqlist <- cbind(freqlist[,1], freqlist_scaled)
+    colnames(freqlist)[1] <- "Speaker"
+    regexResult <- as.data.frame(freqlist[ , grepl( token , names( freqlist ), perl = perl ) ])
+    results <<-  as.data.frame(regexResult, freqlist$Speaker) %>% tibble::rownames_to_column(., "Speaker")
+    if(size >1){
+      regexResult$NewCol <- as.numeric(apply(regexResult[,1:ncol(regexResult)], 1, sum))
+      subset <- cbind(freqlist[,1], regexResult)
+      dataframe <- data.frame("Speaker" = subset[,1], "Token_Scaled" = subset$NewCol, check.names = F)
+    }
+    else{
+      subset <- cbind(freqlist[,1], regexResult)
+      dataframe <- data.frame("Speaker" = subset[,1], "Token_Scaled" = subset[,2], check.names = F)
+    }
+    dataframe2 <-dataframe[order(dataframe$Token, decreasing = T),]
+    dataframe2$var <- dplyr::case_when(grepl("LEM", dataframe2$Speaker) ~ "Lemko", 
+                                       grepl("TRA", dataframe2$Speaker) ~ "Transcarpathian",
+                                       grepl("SLO", dataframe2$Speaker) ~ "Slovak")
+    regexResult2 <- as.data.frame(freqlist1[ , grepl( token , names( freqlist1 ) ) ])
+    
+    if(size >1){
+      regexResult2$NewCol <- as.numeric(apply(regexResult2[,1:ncol(regexResult2)], 1, sum))
+      subset2 <- cbind(freqlist1[,1], regexResult2)
+      names(subset2[,2]) <- token
+      subset2 <- subset(subset2, subset2$NewCol >= 0)
+      dataframe22 <- data.frame("Speaker" = subset2[,1], "Token" = subset2$NewCol, check.names = F)
+      dataframe22 <-dataframe22[order(dataframe22$Token, decreasing = T),]
+      dataframe22$var <- dplyr::case_when(grepl("LEM", dataframe22$Speaker) ~ "Lemko", 
+                                          grepl("TRA", dataframe22$Speaker) ~ "Transcarpathian",
+                                          grepl("SLO", dataframe22$Speaker) ~ "Slovak")
+    }
+    else{
+      subset2 <- cbind(freqlist1[,1], regexResult2)
+      names(subset2[,2]) <- token
+      subset2 <- subset(subset2, subset2[,2] >= 0)
+      dataframe22 <- data.frame("Speaker" = subset[,1], "Token" = subset[,2], check.names = F)
+      dataframe22 <-dataframe[order(dataframe$Token, decreasing = T),]
+      dataframe2$var <- dplyr::case_when(grepl("LEM", dataframe22$Speaker) ~ "Lemko", 
+                                         grepl("TRA", dataframe22$Speaker) ~ "Transcarpathian",
+                                         grepl("SLO", dataframe22$Speaker) ~ "Slovak")
+    }
+    dataframe2 <- cbind(dataframe2, dataframe22$Token)
+    names(dataframe2)[names(dataframe2) == "dataframe22$Token"] <- "Token"
+  }
+  
+  else{
+    regexResult <- as.data.frame(freqlist1[ , grepl( token , names( freqlist1 ), perl = perl )])
+    results <<-  as.data.frame(regexResult, freqlist1$Speaker) %>% tibble::rownames_to_column(., "Speaker")
+    if(size >1){
+      regexResult$NewCol <- as.numeric(apply(regexResult[,1:ncol(regexResult)], 1, sum))
+      subset <- cbind(freqlist1[,1], regexResult)
+      names(subset[,2]) <- token
+      subset <- subset(subset, subset$NewCol >= 0)
+      dataframe <- data.frame("Speaker" = subset[,1], "Token" = subset$NewCol, check.names = F)
+      dataframe2 <-dataframe[order(dataframe$Token, decreasing = T),]
+      dataframe2$var <- dplyr::case_when(grepl("LEM", dataframe2$Speaker) ~ "Lemko", 
+                                         grepl("TRA", dataframe2$Speaker) ~ "Transcarpathian",
+                                         grepl("SLO", dataframe2$Speaker) ~ "Slovak")
+    }
+    else{
+      subset <- cbind(freqlist1[,1], regexResult)
+      names(subset[,2]) <- token
+      subset <- subset(subset, subset[,2] >= 0)
+      dataframe <- data.frame("Speaker" = subset[,1], "Token" = subset[,2], check.names = F)
+      dataframe2 <-dataframe[order(dataframe$Token, decreasing = T),]
+      dataframe2$var <- dplyr::case_when(grepl("LEM", dataframe2$Speaker) ~ "Lemko", 
+                                         grepl("TRA", dataframe2$Speaker) ~ "Transcarpathian",
+                                         grepl("SLO", dataframe2$Speaker) ~ "Slovak")
+    }
+  }
+  if(scale ==T){
+    if(tokenInWords ==""){
+      names(dataframe2)[names(dataframe2) == "Token_Scaled"] <- gsub(" ","", paste(token, "_scaled"))
+      
+    }
+    names(dataframe2)[names(dataframe2) == "Token_Scaled"] <- gsub(" ","", paste(tokenInWords, "_scaled"))
+    if(tokenInWords ==""){
+      names(dataframe2)[names(dataframe2) == "Token"] <- token
+      
+    }
+    names(dataframe2)[names(dataframe2) == "Token"] <- tokenInWords
+  }
+  else{
+    if(tokenInWords ==""){
+      names(dataframe2)[names(dataframe2) == "Token"] <- token
+      
+    }
+    names(dataframe2)[names(dataframe2) == "Token"] <- tokenInWords
+  }
+  return(dataframe2)
+}
+
+znams <- getAllTokenFreqsRegex(type = "w", size = 1, token  = "\\b.*na(l|ú|m|ł)", tokenInWords = "zna.*", scale = T)
+znams <- getTokenFreqsRegex(type = "w", size = 1, token  = "\\b.*na(l|ú|m|ł)", tokenInWords = "zna.*", scale = T)
+
 
